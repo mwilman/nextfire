bfa3af726d662bf0aebba759412f3224de6eebdc
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-02-13 16:13:21 +0100

    added loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import {Inter} from '@next/font/google'
 import React from "react";
-import Link from "next/link";
+import Loader from "@/components/Loader";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
       <div>
-        <Link
-            href={{pathname: '/[username]',
-            query: {username: 'jeffd23'}
-        }}
-            prefetch={false}>
-            <a>Jeff's profile</a>
-        </Link>
+          <Loader show/>
       </div>
   )
 }
